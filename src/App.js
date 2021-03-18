ca70579691b90c1eb28ad7cda6e3d7f01f96de1d
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-03-18 20:52:57 +0100

    test3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>What it this</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
